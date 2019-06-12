25ba79d9cc8cd2b7d3f2875f31e19bcea5e24ce2
paul wisehart <paul@oldcode.org>
2019-06-12 12:07:43 -0400

    R.id.roll_button ..., need to fix the file types of the code from android studio (on windows)

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.android.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll!"
     }
 }
